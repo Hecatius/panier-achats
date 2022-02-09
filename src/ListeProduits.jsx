67c2eb90879ee5bcf7bda570402eaf72e9d3eb65
Jonathan Mores <e1661294@cmaisonneuve.qc.ca>
2022-02-09 15:13:29 -0500

    Implémentation du sass

--- src/ListeProduits.jsx
+++ src/ListeProduits.jsx
@@ -1,16 +1,25 @@
-import'./ListeProduits.css';
+import'./ListeProduits.scss';
+import Produit from'./Produit';
+import lesProduits from './data/produits.json';
+
 export default function ListeProduits() {
+
+  // //Méthode 1 programmation impérative avec une boucle for
+  // let composantsProduits = [];
+  // // Parcourir le tableau les Produits et générer un composant Produit pour chaque élément
+  // for (let i = 0; i < lesProduits.length; i++){
+  //     composantsProduits.push(<Produit nom={lesProduits[i].nom} prix={lesProduits[i].prix} pid={lesProduits[i].id}/>);
+
+  //méthode 2 fonction map
+  
+  
     return (
-    <section className="produits">
+    <section className="ListeProduits">
     <h2>Nos produits</h2>
-    <div>
-        <article>
-          <img src="" alt="" />
-          <div className="titre">Titre du produit</div>
-          <div className="prix">13.95</div>
-          <button>Ajouter au panier</button>
-        </article>
-        
+    <div className="produits">
+        {/* {composantsProduits} */}
+        {lesProduits.map(p => <Produit nom={p.nom} prix={p.prix} pid={p.id}/>)}
+      
     </div>
   </section>
   );
---
+++ src/Produit.jsx
@@ -0,0 +1,12 @@
+import'./Produit.scss';
+
+export default function Produit(props) {
+    return (
+        <article className="Produit">
+          <img src={"images-produits/" + props.pid + ".webp"} alt="T-shirt cool" />
+          <div className="titre">{props.nom}</div>
+          <div className="prix">{props.prix}</div>
+          <button>Ajouter au panier</button>
+        </article>
+    );
+}
