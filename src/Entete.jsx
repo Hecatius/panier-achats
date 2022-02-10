4ffcca923d9930500a2932930d8e97c36290856a
Jonathan Mores <e1661294@cmaisonneuve.qc.ca>
2022-02-10 10:02:03 -0500

    Stage changes???

--- src/Entete.jsx
+++ src/Entete.jsx
@@ -1,11 +1,17 @@
 import './Entete.scss';
+import ShoppingCartSharpIcon from '@mui/icons-material/ShoppingCartSharp';
+import Badge from '@mui/material/Badge';
 
 export default function Entete() {
     return (
         <header className="Entete">
             <h1>Magasin général</h1>
             <nav>
-            <a href="#">Panier d'achats</a>
+            <a href="#">
+            <Badge badgeContent={4} color="primary">
+                <ShoppingCartSharpIcon/>
+            </Badge>
+            </a>
             <a href="#">Contactez-nous</a>
             </nav>
         </header>
