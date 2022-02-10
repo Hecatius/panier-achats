a23d86fc54091c9d760fc6036dbddee02ca62989
Jonathan Mores <e1661294@cmaisonneuve.qc.ca>
2022-02-10 09:59:36 -0500

    Premier usage de Material UI

--- src/BtnAjoutPanier.jsx
+++ src/BtnAjoutPanier.jsx
@@ -1,6 +1,9 @@
 import'./BtnAjoutPanier.scss';
+import Badge from '@mui/material/Badge';
 export default function BtnAjoutPanier(){
     return(
-        <button>Ajouter au panier</button>
+        <Badge badgeContent={4} color="primary">
+            <button className="BtnAjoutPanier">Ajouter au panier</button>
+        </Badge>
     );
 }
